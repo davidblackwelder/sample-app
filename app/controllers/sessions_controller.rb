52724072c8640c23f020611cee2b799b0987c4a1
David Blackwelder <github@davidblackwelder.com>
2020-09-23 06:56:56 -0400

    Add log_in method to the Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      reset_session # Prevents Session Fixation attacks
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
